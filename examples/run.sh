8f5d70ea91176866ce171420f4759e54101d1b81
Petr Benas <pbenas@redhat.com>
2013-07-08 14:41:43 +0200

    remove generated output from examples and place it to gitignore

--- examples/run.sh
+++ examples/run.sh
@@ -24,8 +24,10 @@ SET1=$(find ./xfs -name "*.iozone" | sort)
 #read -ra BASELINE_ARRAY <<< ${BASELINE}
 #read -ra SET1_ARRAY <<< ${SET1}
 
-cp ../src/stylesheet.css .
+cp ../src_new_version/stylesheet.css .
 
-../src/iozone_results_comparator.py --baseline ${BASELINE} --set1 ${SET1} --detail ALL
-../src/iozone_results_comparator.py --baseline ${BASELINE} --set1 ${SET1} 
+#../src/iozone_results_comparator.py --baseline ${BASELINE} --set1 ${SET1} --detail ALL
+../src_new_version/iozone_results_comparator.py --baseline ${BASELINE} --set1 ${SET1} 
+
+rm -f stylesheet.css
 
