4f5fbb4ff546d96eca151d162f8ed939fb465513
Petr Benas <pbenas@redhat.com>
2013-07-24 16:29:33 +0200

    Make the new version the new upstream
    The newer version has much nicer code and the FS and BS oriented graphs are implemented
    using the interactive Google Charts JavaScript plotting library. Detailed mode as well as
    the tab separated values output were deprecated. There was done some kind of experienting
    with the very nice Highcharts plotting library. All plots were implemented using it, if you
    are interrested in, checkout the commit c8e4f799f881. The problem with the Highcharts
    library is it's licence (CC Non-commertial) which prevents this tool to be used in commertial
    environments. This is a huge disadvantage, so all changes related to Highcharts library were
    either reverted or replaced with the Google Charts library.
    
    In the new version, multiset interactive visual comparision, full sparse files (different
    Iozone settings for input files) support and other interresting features will come soon.
    Stay tuned.

--- examples/run.sh
+++ examples/run.sh
@@ -21,13 +21,10 @@
 BASELINE=$(find ./ext4 -name "*.iozone" | sort)
 SET1=$(find ./xfs -name "*.iozone" | sort)
 
-#read -ra BASELINE_ARRAY <<< ${BASELINE}
-#read -ra SET1_ARRAY <<< ${SET1}
+cp ../src/stylesheet.css .
 
-cp ../src_new_version/stylesheet.css .
-
-#../src/iozone_results_comparator.py --baseline ${BASELINE} --set1 ${SET1} --detail ALL
-../src_new_version/iozone_results_comparator.py --baseline ${BASELINE} --set1 ${SET1}
+#../old_version/iozone_results_comparator.py --baseline ${BASELINE} --set1 ${SET1} --detail ALL
+../src/iozone_results_comparator.py --baseline ${BASELINE} --set1 ${SET1}
 
 rm -f stylesheet.css
 
